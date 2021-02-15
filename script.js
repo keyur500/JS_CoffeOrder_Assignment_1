53b63a2e8e39f7e4a0162860d051520dc4e7f870
keyur500 <keyurhedau500@gmail.com>
2021-02-15 13:55:10 -0500

    Constructor Object moved to top

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// Constructor Object 
+function coffeeOrder(name, drinkType, size, drizzle, whippedCreme, sweetner, coldFoam, dairy){
+  this.customerName = name;
+  this.typeOfDrink = drinkType;
+  this.sizeOfCoffee = size;
+  this.drizzle = drizzle;
+  this.whippedCreme = whippedCreme;
+  this.sweetner = sweetner;
+  this.coldFoam = coldFoam;
+  this.dairy = dairy;
+  
+  this.coffeeOrder = function() {
+    document.getElementById("dispOrderName").innerText = this.customerName;
+    document.getElementById("dispSize_Drink").innerText = this.sizeOfCoffee +" | "+ this.typeOfDrink;
+    document.getElementById("dispDairy").innerText = this.dairy;
+    
+    // displaying the selected modifiers only 
+    for(let modifiers in this){
+            if(this[modifiers] == true){
+              let node = document.createElement("li");
+              node.appendChild(document.createTextNode(modifiers));
+              document.getElementById("dispModifiers").appendChild(node);
+            }
+        }
+  };
+}
+
 let orderCoffee = function(){
         
     // Getting the form fields value 
@@ -20,33 +47,6 @@ let orderCoffee = function(){
     let cldFoam = (document.getElementById("coldfoam").checked)? true : false;
     
     let dairy = document.getElementById("dairy").value;
-
-    // Constructor Object 
-    function coffeeOrder(name, drinkType, size, drizzle, whippedCreme, sweetner, coldFoam, dairy){
-      this.customerName = name;
-      this.typeOfDrink = drinkType;
-      this.sizeOfCoffee = size;
-      this.drizzle = drizzle;
-      this.whippedCreme = whippedCreme;
-      this.sweetner = sweetner;
-      this.coldFoam = coldFoam;
-      this.dairy = dairy;
-      
-      this.coffeeOrder = function() {
-        document.getElementById("dispOrderName").innerText = this.customerName;
-        document.getElementById("dispSize_Drink").innerText = this.sizeOfCoffee +" | "+ this.typeOfDrink;
-        document.getElementById("dispDairy").innerText = this.dairy;
-        
-        // displaying the selected modifiers only 
-        for(let modifiers in this){
-               if(this[modifiers] == true){
-                 let node = document.createElement("li");
-                 node.appendChild(document.createTextNode(modifiers));
-                 document.getElementById("dispModifiers").appendChild(node);
-               }
-            }
-      };
-    }
     
     // creating object for coffee order
     let orderObj = new coffeeOrder(customerName, drinkType, DrinkSize, drizzle, whippedCrm, swtner, cldFoam, dairy);
